f31f4a8d32f3e48e2ba3c230c3d92c9d7d943d20
chathumahesha94 <chathumahesha94@gmail.com>
2018-11-10 22:37:56 +0530

    Courses Controller with Index Action

---
+++ CoursesController.cs
@@ -0,0 +1,28 @@
+﻿using StudentRegistrationApplication.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace StudentRegistrationApplication.Controllers
+{
+    public class CoursesController : Controller
+    {
+        // GET: Courses
+        public ViewResult Index()
+        {
+            var courses = GetCourses();
+            return View(courses);
+        }
+        private IEnumerable<Course> GetCourses()
+        {
+            return new List<Course>
+            {
+                new Course{ Id=1,Name="Mathematics and Statistics"},
+                new Course{ Id=2,Name="Database Management System"},
+                new Course{Id=3,Name="Principals of Management"}
+            };
+        }
+    }
+}
