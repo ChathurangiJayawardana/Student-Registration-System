ae9d5b7e69758591b23561c776f39630ccb6c594
chathumahesha94 <chathumahesha94@gmail.com>
2018-11-10 23:51:46 +0530

    Add IndexNo Property to Student Model

--- Student.cs
+++ Student.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
@@ -8,6 +9,14 @@ namespace StudentRegistrationApplication.Models
     public class Student
     {
         public int Id { get; set; }
+        [Required]
+        [StringLength(255)]
         public string Name { get; set; }
+
+        public string IndexNo { get; set; }
+
+        public AcademicType AcademicType { get; set; }
+        public byte AcademicTypeId { get; set; }
+
     }
 }
