b5a1f78bc44f32d48406a487dd446b5c1ab2703a
chathumahesha94 <chathumahesha94@gmail.com>
2018-11-18 18:23:00 +0530

    Add Error Message

--- Student.cs
+++ Student.cs
@@ -9,16 +9,17 @@ namespace StudentRegistrationApplication.Models
     public class Student
     {
         public int Id { get; set; }
-        [Required]
+        [Required(ErrorMessage ="Please enter student's name.")]
         [StringLength(255)]
         public string Name { get; set; }
         [Display(Name="Index Number")]
         public string IndexNo { get; set; }
         [Display(Name="Register Date")]
         public DateTime? RegisterDate { get; set; }
+        
         [Display(Name="Degree Name")]
         public AcademicType AcademicType { get; set; }
-      
+        [Required(ErrorMessage = "Please enter Degree Name")]
         public byte AcademicTypeId { get; set; }
     }
 }
