3d160a5fe902cacb7b7e39dfc14502d88299752e
chathumahesha94 <chathumahesha94@gmail.com>
2018-11-25 22:16:52 +0530

    Api StudentsController

---
+++ Api.StudentsController.cs
@@ -0,0 +1,75 @@
+﻿using StudentRegistrationApplication.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace StudentRegistrationApplication.Controllers.Api
+{
+    public class StudentsController : ApiController
+    {
+        private ApplicationDbContext _context;
+
+        public StudentsController()
+        {
+            _context = new ApplicationDbContext();
+        }
+        // GET /api/students
+        public IEnumerable<Student>GetStudents()
+        {
+            return _context.Students.ToList();
+        }
+
+        //GET /api/studentrs/1
+        public Student GetStudent(int id)
+        {
+            var student = _context.Students.SingleOrDefault(c => c.Id == id);
+
+            if (student == null)
+                throw new HttpResponseException(HttpStatusCode.NotFound);
+
+            return student;
+        }
+
+        //POST /api/students
+        [HttpPost]
+        public Student CreateStudent(Student student)
+        {
+            if (!ModelState.IsValid)
+                throw new HttpResponseException(HttpStatusCode.BadRequest);
+            _context.Students.Add(student);
+            _context.SaveChanges();
+
+            return student;
+        }
+        //PUT /api/students/1
+        public void UpdateStudent(int id,Student student)
+        {
+            if (!ModelState.IsValid)
+                throw new HttpResponseException(HttpStatusCode.BadRequest);
+
+            var studentInDb = _context.Students.SingleOrDefault(c => c.Id == id);
+            if (studentInDb == null)
+                throw new HttpResponseException(HttpStatusCode.NotFound);
+            studentInDb.Name = student.Name;
+            studentInDb.IndexNo = student.IndexNo;
+            studentInDb.RegisterDate = student.RegisterDate;
+            studentInDb.AcademicTypeId = student.AcademicTypeId;
+
+            _context.SaveChanges();
+        }
+        // DELETE /api/students/1
+        [HttpDelete]
+        public void DeleteStudent(int id)
+        {
+            var studentInDb = _context.Students.SingleOrDefault(c => c.Id == id);
+            if (studentInDb == null)
+                throw new HttpResponseException(HttpStatusCode.NotFound);
+
+            _context.Students.Remove(studentInDb);
+            _context.SaveChanges();
+        }
+    }
+}
