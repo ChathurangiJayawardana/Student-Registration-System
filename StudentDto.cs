68e0f20ae16f825faf155a2c9d3a3add32a6fa68
chathumahesha94 <chathumahesha94@gmail.com>
2018-11-27 00:51:36 +0530

    StudentDto

--- StudentDto.cs
+++ StudentDto.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using StudentRegistrationApplication.Models;
+using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
@@ -7,5 +9,15 @@ namespace StudentRegistrationApplication.Dtos
 {
     public class StudentDto
     {
+        public int Id { get; set; }
+        [Required]
+        [StringLength(255)]
+        public string Name { get; set; }
+    
+        public string IndexNo { get; set; }
+        
+        public DateTime? RegisterDate { get; set; }
+
+        public byte AcademicTypeId { get; set; }
     }
 }
